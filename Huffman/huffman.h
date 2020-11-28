2ad109feea27ee3026f323ad753e2be69c00e92c
CamillePld <73800852+CamillePld@users.noreply.github.com>
2020-11-28 23:56:30 +0100

    modification huffman.h

--- Huffman/huffman.h
+++ Huffman/huffman.h
@@ -1,7 +1,7 @@
 #ifndef HUFFMAN_H
 #define HUFFMAN_H
-#include "arbre.h"
-#include "liste.h"
+#include "../Types/arbre.h"
+#include "../Types/liste.h"
 extern void caractere_binaire(char ch, char bit[9]);
 extern void ecriture_texte(char *fichier1, char *fichier2);
 
