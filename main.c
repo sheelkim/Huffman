8c6d4437b79120e7b0021be9285ceb25a9fa9eda
Kim Ly TEK <kim.ly.tek@efrei.net>
2020-12-10 12:21:20 +0100

    changes

--- main.c
+++ main.c
@@ -4,14 +4,6 @@
 
 int main()
 {
-<<<<<<< HEAD
-    printf("Success");
-=======
-    ecriture_texte("texte.txt", "binaire.txt");
-    printf("Nombre de caractère dans le fichier texte.txt : %d\n", compter_caractere("texte.txt"));
-    printf("Nombre de caractère dans le fichier binaire : %d\n", compter_caractere("binaire.txt"));
-
->>>>>>> 719fb07f86f37ec9a1fd461b3742fb90490cfe98
     return 0;
 }
 
