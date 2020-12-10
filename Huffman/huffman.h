26cf7553bd30311337dbe26c7ed498c4c15488ce
Kim Ly TEK <kim.ly.tek@efrei.net>
2020-12-10 11:38:23 +0100

    Doxygen

--- Huffman/huffman.h
+++ Huffman/huffman.h
@@ -1,12 +1,49 @@
+/*********************************************************************************************
+ * \file huffman.h
+ * \brief Header of the library allowing the management of huffman
+ * 
+ * \autor : Kim Ly TEK, Chloe ANGLARD, Camille BENEZECH, Camille PALLUD
+ * \date : december 2020
+ * ********************************************************************************************/
+
 #ifndef HUFFMAN_H
 #define HUFFMAN_H
 #include "../Types/arbre.h"
 #include "../Types/liste.h"
-
+/** 
+ * \brief Function to convert char to binary
+ * \return nothing
+ */
 extern void caractere_binaire(char ch, char bit[9]);
+
+/** 
+ * \brief Function to write the conversion of a texte in binary in a text file 
+ * \return nothing
+ */
 extern void ecriture_texte(char *fichier1, char *fichier2);
+
+/** 
+ * \brief Function tp count the character of a file
+ * \return nothing
+ */
 extern int compter_caractere(char *fichier);
+
+/** 
+ * \brief Function to see the occurences of a text
+ * \return a list
+ */
 extern Liste occurrence_texte(char *fichier);
+
+/** 
+ * \brief Function to create a tree 
+ * \return a tree
+ */
 extern Arbre arbre_huffman(Liste liste);
+
+/** 
+ * \brief Function to find the smallest element of a list
+ * \return a node
+ */
+extern Noeud* petit_element(Liste *liste);
 
 #endif // HUFFMAN_H
